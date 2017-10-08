d36abb6beeb0043c0b3ec8094bdc1d98f4bd262c
Darrel Miller <darrmi@microsoft.com>
2017-10-08 16:04:51 +0200

    updated build process to build two nupkg

--- build.cmd
+++ build.cmd
@@ -1,4 +1,10 @@
+SET VERSION=0.9.1
 SET PROJ=%~dp0src\OpenApi\OpenApi.csproj 
 msbuild %PROJ% /t:restore /p:Configuration=Release
 msbuild %PROJ% /t:build /p:Configuration=Release
-msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts
+msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts;Version=%VERSION%
+
+SET PROJ=%~dp0src\OpenApi.Readers\OpenApi.Readers.csproj 
+msbuild %PROJ% /t:restore /p:Configuration=Release
+msbuild %PROJ% /t:build /p:Configuration=Release
+msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts;Version=%VERSION%
