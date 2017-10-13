1cac51f46604dcf895065590d695372c486c9cdc
Sam <saxu@microsoft.com>
2017-10-13 13:48:29 -0700

    Adjust the project strucutre
    1. Move the test project to test folder
    2. Change the project name

--- build.cmd
+++ build.cmd
@@ -1,10 +1,10 @@
 SET VERSION=0.9.1
-SET PROJ=%~dp0src\OpenApi\OpenApi.csproj 
+SET PROJ=%~dp0src\Microsoft.OpenApi.Core\Microsoft.OpenApi.Core.csproj 
 msbuild %PROJ% /t:restore /p:Configuration=Release
 msbuild %PROJ% /t:build /p:Configuration=Release
 msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts;Version=%VERSION%
 
-SET PROJ=%~dp0src\OpenApi.Readers\OpenApi.Readers.csproj 
+SET PROJ=%~dp0src\Microsoft.OpenApi.Readers\Microsoft.OpenApi.Readers.csproj 
 msbuild %PROJ% /t:restore /p:Configuration=Release
 msbuild %PROJ% /t:build /p:Configuration=Release
 msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts;Version=%VERSION%
