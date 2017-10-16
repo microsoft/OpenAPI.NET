2f0f385fcd90e689964d96ecdd8da622a3f65912
Sam <saxu@microsoft.com>
2017-10-16 11:24:55 -0700

    move the open api object classes to models subfolder

--- src/Microsoft.OpenApi/Writers/IOpenApiStructureWriter.cs
+++ src/Microsoft.OpenApi/Writers/IOpenApiStructureWriter.cs
@@ -8,7 +8,29 @@ namespace Microsoft.OpenApi.Writers
 {
     using System.IO;
     using Microsoft.OpenApi;
-    public interface IOpenApiStructureWriter {
+    public interface IOpenApiStructureWriter
+    {
         void Write(Stream stream, OpenApiDocument document);
+    }
+
+    public interface IOpenApiDocumentSerializer
+    {
+        bool Save(OpenApiDocument document);
+    }
+
+    public class OpenApiV3Serializer : IOpenApiDocumentSerializer
+    {
+        public bool Save(OpenApiDocument document)
+        {
+            return true;
+        }
+    }
+
+    public class OpenApiV2Serializer : IOpenApiDocumentSerializer
+    {
+        public bool Save(OpenApiDocument document)
+        {
+            return true;
+        }
     }
 }
