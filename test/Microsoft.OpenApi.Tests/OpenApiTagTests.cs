d201d6c8f82b71ccd9a01cdedeb69ac6e9b254a9
Sam <saxu@microsoft.com>
2017-10-16 10:35:46 -0700

    small changes to OpenApiTag class and start to add test cases

---
+++ test/Microsoft.OpenApi.Tests/OpenApiTagTests.cs
@@ -0,0 +1,19 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="OpenApiTagTests.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+namespace Microsoft.OpenApi.Tests
+{
+    public class OpenApiTagTests
+    {
+        public static OpenApiTag BasicTag = new OpenApiTag();
+        public static OpenApiTag AdvanceTag = new OpenApiTag()
+        {
+            Name = "pet",
+            Description = "Pets operations",
+            ExternalDocs = OpenApiExternalDocsTests.AdvanceExDocs
+        };
+    }
+}
