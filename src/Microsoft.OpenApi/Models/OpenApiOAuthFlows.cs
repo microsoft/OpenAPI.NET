e933c84944a5880a9c05474a11e28b47f5b44d3e
Sam Xu <saxu@microsoft.com>
2017-10-23 13:00:35 -0700

    Add more missed objects in core

---
+++ src/Microsoft.OpenApi/Models/OpenApiOAuthFlows.cs
@@ -0,0 +1,41 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="OpenApiOAuthFlows.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.OpenApi
+{
+    /// <summary>
+    /// OAuth Flows Object.
+    /// </summary>
+    public class OpenApiOAuthFlows : IOpenApiExtension
+    {
+        /// <summary>
+        /// Configuration for the OAuth Implicit flow
+        /// </summary>
+        public OpenApiOAuthFlow Implicit { get; set; }
+
+        /// <summary>
+        /// Configuration for the OAuth Resource Owner Password flow.
+        /// </summary>
+        public OpenApiOAuthFlow Password { get; set; }
+
+        /// <summary>
+        /// Configuration for the OAuth Client Credentials flow.
+        /// </summary>
+        public OpenApiOAuthFlow ClientCredentials { get; set; }
+
+        /// <summary>
+        /// Configuration for the OAuth Authorization Code flow.
+        /// </summary>
+        public OpenApiOAuthFlow AuthorizationCode { get; set; }
+
+        /// <summary>
+        /// Specification Extensions.
+        /// </summary>
+        public IDictionary<string, IOpenApiAny> Extensions { get; set; }
+    }
+}
--- src/Microsoft.OpenApi/Models/OpenApiSecurityScheme.cs
+++ src/Microsoft.OpenApi/Models/OpenApiSecurityScheme.cs
@@ -27,6 +27,14 @@ namespace Microsoft.OpenApi
         public Uri TokenUrl { get; set; }
         public IDictionary<string,string> Scopes { get; set; }
 
+        /// <summary>
+        /// REQUIRED. An object containing configuration information for the flow types supported.
+        /// </summary>
+        public OpenApiOAuthFlows Flows { get; set; }
+
+        /// <summary>
+        /// Specification Extensions.
+        /// </summary>
         public IDictionary<string, IOpenApiAny> Extensions { get; set; }
 
         public OpenApiReference Pointer
