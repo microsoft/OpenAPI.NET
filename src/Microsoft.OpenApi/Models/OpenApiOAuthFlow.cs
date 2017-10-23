415f6076d9797d6c50b2e66a2d0422ec43a2c4a5
Sam Xu <saxu@microsoft.com>
2017-10-23 13:00:35 -0700

    Add more missed objects in core

---
+++ src/Microsoft.OpenApi/Models/OpenApiOAuthFlow.cs
@@ -0,0 +1,42 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="OpenApiOAuthFlow.cs" company="Microsoft">
+//      Copyright (C) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+// </copyright>
+//---------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.OpenApi
+{
+    /// <summary>
+    /// OAuth Flow Object.
+    /// </summary>
+    public class OpenApiOAuthFlow : IOpenApiExtension
+    {
+        /// <summary>
+        /// REQUIRED. The authorization URL to be used for this flow.
+        /// </summary>
+        public Uri AuthorizationUrl { get; set; }
+
+        /// <summary>
+        /// REQUIRED. The token URL to be used for this flow.
+        /// </summary>
+        public Uri TokenUrl { get; set; }
+
+        /// <summary>
+        /// The URL to be used for obtaining refresh tokens.
+        /// </summary>
+        public Uri RefreshUrl { get; set; }
+
+        /// <summary>
+        /// REQUIRED. A map between the scope name and a short description for it.
+        /// </summary>
+        public IDictionary<string, string> Scopes { get; set; }
+
+        /// <summary>
+        /// Specification Extensions.
+        /// </summary>
+        public IDictionary<string, IOpenApiAny> Extensions { get; set; }
+    }
+}
