474348f4864efe1ba4e3aefd0f2459f564a17d1e
Sam Xu <saxu@microsoft.com>
2017-10-31 09:55:16 -0700

    add OpenApiEncoding class and tests, modify OpenApiExternalDoc

---
+++ src/Microsoft.OpenApi/Models/OpenApiEncoding.cs
@@ -0,0 +1,97 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Writers;
+
+namespace Microsoft.OpenApi.Models
+{
+    /// <summary>
+    /// ExternalDocs object.
+    /// </summary>
+    public class OpenApiEncoding : OpenApiElement, IOpenApiExtension
+    {
+        /// <summary>
+        /// The Content-Type for encoding a specific property.
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// A map allowing additional information to be provided as headers.
+        /// </summary>
+        public IDictionary<string, OpenApiHeader> Headers { get; set; }
+
+        /// <summary>
+        /// Describes how a specific property value will be serialized depending on its type. 
+        /// </summary>
+        public ParameterStyle? Style { get; set; }
+
+        /// <summary>
+        /// Explode
+        /// </summary>
+        public bool? Explode { get; set; }
+
+        /// <summary>
+        /// AllowReserved
+        /// </summary>
+        public bool? AllowReserved { get; set; }
+
+        /// <summary>
+        /// This object MAY be extended with Specification Extensions.
+        /// </summary>
+        public IDictionary<string, IOpenApiAny> Extensions { get; set; }
+
+        /// <summary>
+        /// Serialize <see cref="OpenApiExternalDocs"/> to Open Api v3.0.
+        /// </summary>
+        internal override void WriteAsV3(IOpenApiWriter writer)
+        {
+            if (writer == null)
+            {
+                throw Error.ArgumentNull("writer");
+            }
+
+            // { for json, empty for YAML
+            writer.WriteStartObject();
+
+            // contentType
+            writer.WriteStringProperty(OpenApiConstants.OpenApiDocContentType, ContentType);
+
+            // headers
+            writer.WriteMap(OpenApiConstants.OpenApiDocHeaders, Headers, (w, h) => h.WriteAsV3(w));
+
+            // style
+            writer.WriteStringProperty(OpenApiConstants.OpenApiDocStyle, Style?.ToString());
+
+            // explode
+            if (Explode != null)
+            {
+                writer.WriteBoolProperty(OpenApiConstants.OpenApiDocExplode, Explode.Value, false);
+            }
+
+            // allowReserved
+            if (AllowReserved != null)
+            {
+                writer.WriteBoolProperty(OpenApiConstants.OpenApiDocAllowReserved, AllowReserved.Value, false);
+            }
+
+            // specification extensions
+            writer.WriteExtensions(Extensions);
+
+            // } for json, empty for YAML
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Serialize <see cref="OpenApiExternalDocs"/> to Open Api v2.0.
+        /// </summary>
+        internal override void WriteAsV2(IOpenApiWriter writer)
+        {
+            // nothing here
+        }
+    }
+}
---
+++ test/Microsoft.OpenApi.Tests/Models/OpenApiEncodingTests.cs
@@ -0,0 +1,69 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.OpenApi.Models.Tests
+{
+    public class OpenApiEncodingTests
+    {
+        public static OpenApiEncoding BasicEncoding = new OpenApiEncoding();
+        public static OpenApiEncoding AdvanceEncoding = new OpenApiEncoding()
+        {
+            ContentType = "image/png, image/jpeg",
+            Style = ParameterStyle.simple,
+            Explode = true,
+            AllowReserved = true,
+        };
+
+        [Theory]
+        [InlineData(OpenApiFormat.Json, "{ }")]
+        [InlineData(OpenApiFormat.Yaml, "")]
+        public void SerializeBasicEncodingAsV3Works(OpenApiFormat format, string expect)
+        {
+            // Arrange & Act
+            string actual = BasicEncoding.Serialize(OpenApiSpecVersion.OpenApi3_0, format);
+
+            // Assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void SerializeAdvanceEncodingAsV3JsonWorks()
+        {
+            // Arrange
+            string expect = @"
+{
+  ""contentType"": ""image/png, image/jpeg"",
+  ""style"": ""simple"",
+  ""explode"": true,
+  ""allowReserved"": true
+}";
+
+            // Act
+            string actual = AdvanceEncoding.SerializeAsJson();
+
+            // Assert
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void SerializeAdvanceEncodingAsV3YamlWorks()
+        {
+            // Arrange
+            string expect = @"
+contentType: image/png, image/jpeg
+style: simple
+explode: true
+allowReserved: true";
+
+            // Act
+            string actual = AdvanceEncoding.SerializeAsYaml();
+
+            // Assert
+            Assert.Equal(expect, actual);
+        }
+    }
+}
