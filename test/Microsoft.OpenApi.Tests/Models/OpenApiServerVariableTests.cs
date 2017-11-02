8fa7b1732a81a8a1e67c52e3c2700a82b1a34adf
Sam Xu <saxu@microsoft.com>
2017-11-02 14:07:29 -0700

    Modify OpenApiServer and OpenApiServerVariable and add tests

---
+++ test/Microsoft.OpenApi.Tests/Models/OpenApiServerVariableTests.cs
@@ -0,0 +1,78 @@
+﻿// ------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// ------------------------------------------------------------
+
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.OpenApi.Models;
+using Xunit;
+
+namespace Microsoft.OpenApi.Tests.Models
+{
+    public class OpenApiServerVariableTests
+    {
+        public static OpenApiServerVariable BasicVariable = new OpenApiServerVariable();
+        public static OpenApiServerVariable AdvanceVariable = new OpenApiServerVariable()
+        {
+            Default = "8443",
+            Enum = new List<string>
+            {
+                "8443",
+                "443"
+            },
+            Description = "test description"
+        };
+
+        [Theory]
+        [InlineData(OpenApiFormat.Json, "{ }")]
+        [InlineData(OpenApiFormat.Yaml, "")]
+        public void SerializeBasicServerVariableAsV3Works(OpenApiFormat format, string expect)
+        {
+            // Arrange & Act
+            string actual = BasicVariable.Serialize(OpenApiSpecVersion.OpenApi3_0, format);
+
+            // Assert
+            actual.Should().Be(expect);
+        }
+
+        [Fact]
+        public void SerializeAdvanceServerVariableAsV3JsonWorks()
+        {
+            // Arrange
+            string expect = 
+@"{
+  ""default"": ""8443"",
+  ""description"": ""test description"",
+  ""enum"": [
+    ""8443"",
+    ""443""
+  ]
+}";
+
+            // Act
+            string actual = AdvanceVariable.SerializeAsJson();
+
+            // Assert
+            actual.Should().Be(expect);
+        }
+
+        [Fact]
+        public void SerializeAdvanceServerVariableAsV3YamlWorks()
+        {
+            // Arrange
+            string expect = 
+@"default: 8443
+description: test description
+enum:
+  - 8443
+  - 443";
+
+            // Act
+            string actual = AdvanceVariable.SerializeAsYaml();
+
+            // Assert
+            actual.Should().Be(expect);
+        }
+    }
+}
