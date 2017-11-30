4b4817f6007a870ff80b4c5e97faa22d70ac7070
PerthCharern <perthcha@microsoft.com>
2017-11-30 15:17:15 -0800

    - Null object in collections is handled explicitly. Otherwise, operating an action on null (e.g. calling SerializeAsV3) has potential to yield null reference exception.
      - This is sort of a grey area since we will only encounter this for a "broken" object (e.g. a null schema in the Schemas map in Components - see the written unit tests), but I think it's a good idea to have this safeguard. Getting a generic null reference exception is a bad experience for our customer in any situation, I believe.
    
    - Unit test for Components

--- test/Microsoft.OpenApi.Tests/Models/OpenApiComponentsTests.cs
+++ test/Microsoft.OpenApi.Tests/Models/OpenApiComponentsTests.cs
@@ -3,12 +3,241 @@
 //  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // ------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.OpenApi.Tests.Models
 {
     public class OpenApiComponentsTests
     {
-        public static OpenApiComponents basicComponents = new OpenApiComponents();
+        public static OpenApiComponents AdvancedComponents = new OpenApiComponents
+        {
+            Schemas = new Dictionary<string, OpenApiSchema>
+            {
+                ["schema1"] = new OpenApiSchema
+                {
+                    Properties = new Dictionary<string, OpenApiSchema>
+                    {
+                        ["property2"] = new OpenApiSchema
+                        {
+                            Type = "integer"
+                        },
+                        ["property3"] = new OpenApiSchema
+                        {
+                            Type = "string",
+                            MaxLength = 15
+                        }
+                    },
+                },
+            },
+            SecuritySchemes = new Dictionary<string, OpenApiSecurityScheme>
+            {
+                ["securityScheme1"] = new OpenApiSecurityScheme
+                {
+                    Description = "description1",
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            Scopes = new Dictionary<string, string>
+                            {
+                                ["operation1:object1"] = "operation 1 on object 1",
+                                ["operation2:object2"] = "operation 2 on object 2"
+                            },
+                            AuthorizationUrl = new Uri("https://example.com/api/oauth")
+                        }
+                    }
+                },
+                ["securityScheme2"] = new OpenApiSecurityScheme
+                {
+                    Description = "description1",
+                    Type = SecuritySchemeType.OpenIdConnect,
+                    Scheme = "openIdConnectUrl",
+                    OpenIdConnectUrl = new Uri("https://example.com/openIdConnect")
+                }
+            }
+        };
+
+        public static OpenApiComponents BasicComponents = new OpenApiComponents();
+
+        public static OpenApiComponents BrokenComponents = new OpenApiComponents
+        {
+            Schemas = new Dictionary<string, OpenApiSchema>
+            {
+                ["schema1"] = new OpenApiSchema
+                {
+                    Type = "string"
+                },
+                ["schema2"] = null
+            }
+        };
+
+        private readonly ITestOutputHelper _output;
+
+        public OpenApiComponentsTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void SerializeBasicComponentsAsJsonWorks()
+        {
+            // Arrange
+            var expected = @"{ }";
+
+            // Act
+            var actual = BasicComponents.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SerializeBasicComponentsAsYamlWorks()
+        {
+            // Arrange
+            var expected = @"{ }";
+
+            // Act
+            var actual = BasicComponents.SerializeAsYaml(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SerializeAdvancedComponentsAsJsonWorks()
+        {
+            // Arrange
+            var expected = @"{
+  ""schemas"": {
+    ""schema1"": {
+      ""properties"": {
+        ""property2"": {
+          ""type"": ""integer""
+        },
+        ""property3"": {
+          ""maxLength"": 15,
+          ""type"": ""string""
+        }
+      }
+    }
+  },
+  ""securitySchemes"": {
+    ""securityScheme1"": {
+      ""type"": ""oauth2"",
+      ""description"": ""description1"",
+      ""flows"": {
+        ""implicit"": {
+          ""authorizationUrl"": ""https://example.com/api/oauth"",
+          ""scopes"": {
+            ""operation1:object1"": ""operation 1 on object 1"",
+            ""operation2:object2"": ""operation 2 on object 2""
+          }
+        }
+      }
+    },
+    ""securityScheme2"": {
+      ""type"": ""openIdConnect"",
+      ""description"": ""description1"",
+      ""openIdConnectUrl"": ""https://example.com/openIdConnect""
+    }
+  }
+}";
+
+            // Act
+            var actual = AdvancedComponents.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SerializeAdvancedComponentsAsYamlWorks()
+        {
+            // Arrange
+            var expected = @"schemas:
+  schema1:
+    properties:
+      property2:
+        type: integer
+      property3:
+        maxLength: 15
+        type: string
+securitySchemes:
+  securityScheme1:
+    type: oauth2
+    description: description1
+    flows:
+      implicit:
+        authorizationUrl: https://example.com/api/oauth
+        scopes:
+          operation1:object1: operation 1 on object 1
+          operation2:object2: operation 2 on object 2
+  securityScheme2:
+    type: openIdConnect
+    description: description1
+    openIdConnectUrl: https://example.com/openIdConnect";
+
+            // Act
+            var actual = AdvancedComponents.SerializeAsYaml(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SerializeBrokenComponentsAsJsonWorks()
+        {
+            // Arrange
+            var expected = @"{
+  ""schemas"": {
+    ""schema1"": {
+      ""type"": ""string""
+    },
+    ""schema2"": null
+  }
+}";
+
+            // Act
+            var actual = BrokenComponents.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void SerializeBrokenComponentsAsYamlWorks()
+        {
+            // Arrange
+            var expected = @"schemas:
+  schema1:
+    type: string
+  schema2: ";
+
+            // Act
+            var actual = BrokenComponents.SerializeAsYaml(OpenApiSpecVersion.OpenApi3_0_0);
+
+            // Assert
+            actual = actual.MakeLineBreaksEnvironmentNeutral();
+            expected = expected.MakeLineBreaksEnvironmentNeutral();
+            actual.Should().Be(expected);
+        }
     }
 }
