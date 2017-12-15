31a3cd9b71e3407d46842ea1a8376d04961ef8ba
Sam Xu <saxu@microsoft.com>
2017-12-15 15:09:06 -0800

    Add test cases for external docs and response

---
+++ test/Microsoft.OpenApi.Tests/Validations/OpenApiExternalDocsValidationTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. 
+
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Validations;
+using Xunit;
+
+namespace Microsoft.OpenApi.Tests.Models
+{
+    public class OpenApiExternalDocsValidationTests
+    {
+        [Fact]
+        public void ValidateUrlIsRequiredInExternalDocs()
+        {
+            // Arrange
+            IEnumerable<ValidationError> errors;
+            OpenApiExternalDocs externalDocs = new OpenApiExternalDocs();
+
+            // Act
+            bool result = externalDocs.Validate(out errors);
+
+            // Assert
+            Assert.False(result);
+            Assert.NotNull(errors);
+            ValidationError error = Assert.Single(errors);
+            Assert.Equal("The field 'url' in 'External Documentation' object is REQUIRED.", error.ErrorMessage);
+        }
+    }
+}
---
+++ test/Microsoft.OpenApi.Tests/Validations/OpenApiResponseValidationTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. 
+
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Validations;
+using Xunit;
+
+namespace Microsoft.OpenApi.Tests.Models
+{
+    public class OpenApiResponseValidationTests
+    {
+        [Fact]
+        public void ValidateDescriptionIsRequiredInResponse()
+        {
+            // Arrange
+            IEnumerable<ValidationError> errors;
+            OpenApiResponse response = new OpenApiResponse();
+
+            // Act
+            bool result = response.Validate(out errors);
+
+            // Assert
+            Assert.False(result);
+            Assert.NotNull(errors);
+            ValidationError error = Assert.Single(errors);
+            Assert.Equal("The field 'description' in 'response' object is REQUIRED.", error.ErrorMessage);
+        }
+    }
+}
