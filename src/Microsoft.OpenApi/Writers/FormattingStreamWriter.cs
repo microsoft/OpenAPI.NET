044ddde5a10b408ab87c1314821bf7fd65742958
Brecht Debaere <Brecht.Debaere@spray.com>
2019-02-27 00:58:27 +0100

    Add comments.

--- src/Microsoft.OpenApi/Writers/FormattingStreamWriter.cs
+++ src/Microsoft.OpenApi/Writers/FormattingStreamWriter.cs
@@ -1,20 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.OpenApi.Writers
 {
+    /// <summary>
+    /// A custom <see cref="StreamWriter"/> which supports setting a <see cref="IFormatProvider"/>.
+    /// </summary>
     public class FormattingStreamWriter : StreamWriter
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormattingStreamWriter"/> class.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="formatProvider"></param>
         public FormattingStreamWriter(Stream stream, IFormatProvider formatProvider)
             : base(stream)
         {
             this.FormatProvider = formatProvider;
         }
 
+        /// <summary>
+        /// The <see cref="IFormatProvider"/> associated with this <see cref="FormattingStreamWriter"/>.
+        /// </summary>
         public override IFormatProvider FormatProvider { get; }
     }
 }
