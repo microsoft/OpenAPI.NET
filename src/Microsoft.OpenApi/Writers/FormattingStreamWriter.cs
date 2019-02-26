af3abd52ab910414226f435199807f901aaa59c2
Brecht Debaere <Brecht.Debaere@spray.com>
2019-02-27 00:54:49 +0100

    Add culture invariant writer.

---
+++ src/Microsoft.OpenApi/Writers/FormattingStreamWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OpenApi.Writers
+{
+    public class FormattingStreamWriter : StreamWriter
+    {
+        public FormattingStreamWriter(Stream stream, IFormatProvider formatProvider)
+            : base(stream)
+        {
+            this.FormatProvider = formatProvider;
+        }
+
+        public override IFormatProvider FormatProvider { get; }
+    }
+}
