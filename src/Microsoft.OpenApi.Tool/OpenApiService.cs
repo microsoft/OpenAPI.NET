9f715d4c6ccf6fb7f34296fbb57b722f86ab5937
darrelmiller <darrel.miller@microsoft.com>
2020-01-20 16:59:14 -0500

    Initial implementation of OpenAPI cmdline tool

---
+++ src/Microsoft.OpenApi.Tool/OpenApiService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Validations;
+using Microsoft.OpenApi.Writers;
+
+namespace Microsoft.OpenApi.Tool
+{
+    static class OpenApiService
+    {
+        public static void ProcessOpenApiDocument(
+            FileInfo fileOption,
+            string outputPath,
+            OpenApiSpecVersion version,
+            OpenApiFormat format,
+            bool inline = false)
+        {
+                Stream stream = fileOption.OpenRead();
+
+                var document = new OpenApiStreamReader(new OpenApiReaderSettings
+                {
+                    ReferenceResolution = ReferenceResolutionSetting.ResolveLocalReferences,
+                    RuleSet = ValidationRuleSet.GetDefaultRuleSet()
+                }
+                ).Read(stream, out var context);
+
+                if (context.Errors.Count != 0)
+                {
+                    var errorReport = new StringBuilder();
+
+                    foreach (var error in context.Errors)
+                    {
+                        errorReport.AppendLine(error.ToString());
+                    }
+
+                    throw new ArgumentException(String.Join(Environment.NewLine, context.Errors.Select(e => e.Message).ToArray()));
+            }
+
+            using (var outputStream = new FileStream(outputPath, FileMode.Create))
+            {
+                document.Serialize(
+                   outputStream,
+                   version,
+                   format,
+                   new OpenApiWriterSettings()
+                   {
+                       ReferenceInline = inline == true ? ReferenceInlineSetting.InlineLocalReferences : ReferenceInlineSetting.DoNotInlineReferences
+                   });
+
+                outputStream.Position = 0;
+                outputStream.Flush();
+            }
+        }
+}
+}
---
+++ src/Microsoft.OpenApi.Tool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using Microsoft.OpenApi;
+
+namespace Microsoft.OpenApi.Tool
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Option(
+                    "--input",
+                    "Input OpenAPI description")
+                {
+                    Argument = new Argument<FileInfo>()
+                },
+                new Option(
+                    "--output",
+                    "Output path for OpenAPI Description")
+                {
+                    Argument = new Argument<string>()
+                },
+                new Option(
+                    "--output-version",
+                    "OpenAPI Version")
+                {
+                    Argument = new Argument<OpenApiSpecVersion>(() => OpenApiSpecVersion.OpenApi3_0)
+                },
+                new Option(
+                    "--output-format",
+                    "OpenAPI format [Json | Yaml")
+                {
+                    Argument = new Argument<OpenApiFormat>(() => OpenApiFormat.Yaml )
+                }
+            };
+
+            rootCommand.Description = "OpenAPI";
+
+            rootCommand.Handler = CommandHandler.Create<FileInfo,string,OpenApiSpecVersion,OpenApiFormat, bool>(
+               OpenApiService.ProcessOpenApiDocument);
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
+    }
+}
