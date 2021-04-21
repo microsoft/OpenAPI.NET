9e7efcca3346b4034867780650a79d98a8519aac
Irvine Sunday <irochand@microsoft.com>
2021-04-21 18:48:54 +0300

    Remove unnecessary usings

--- src/Microsoft.OpenApi/Extensions/StringExtensions.cs
+++ src/Microsoft.OpenApi/Extensions/StringExtensions.cs
@@ -1,8 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. 
+// Licensed under the MIT license.
 
 using System;
-using System.Linq;
 using System.Reflection;
 using Microsoft.OpenApi.Attributes;
 
