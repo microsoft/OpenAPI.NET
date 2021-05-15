05a1d77b3b39be7b7f26204fb67c02b232a99e03
Darrel Miller <darrel.miller@microsoft.com>
2021-05-15 14:02:57 -0400

    Removed DataTypeMismatch as default rule

--- src/Microsoft.OpenApi/Validations/Rules/OpenApiMediaTypeRules.cs
+++ src/Microsoft.OpenApi/Validations/Rules/OpenApiMediaTypeRules.cs
@@ -8,7 +8,16 @@ namespace Microsoft.OpenApi.Validations.Rules
     /// <summary>
     /// The validation rules for <see cref="OpenApiMediaType"/>.
     /// </summary>
-    [OpenApiRule]
+    /// <remarks>
+    /// Removed this in v1.3 as a default rule as the OpenAPI specification does not require that example
+    /// values validate against the schema.  Validating examples against the schema is particularly difficult
+    /// as it requires parsing of the example using the schema as a guide.  This is not possible when the schema
+    /// is ref'd.  Even if we fix this issue, this rule should be treated as a warning, not an error
+    /// Future versions of the validator should make that distinction.
+    /// Future versions of the example parsers should not try an infer types.
+    /// Example validation should be done as a separate post reading step so all schemas can be fully available.
+    /// </remarks>
+    //[OpenApiRule]
     public static class OpenApiMediaTypeRules
     {
         /// <summary>
