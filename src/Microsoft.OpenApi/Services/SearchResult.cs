73bef228c86b86abf96043855ecb981c2f39e720
Maggie Kimani <maggiekim42@gmail.com>
2021-10-18 12:38:06 +0300

    Add a search result object model

---
+++ src/Microsoft.OpenApi/Services/SearchResult.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.OpenApi.Services
+{
+    public class SearchResult
+    {
+        public CurrentKeys CurrentKeys { get; set; }
+        public OpenApiOperation Operation { get; set; }
+    }
+}
