fb5512ce6cfba4e0d1a0891d607ffe8958051c0a
Maggie Kimani <maggiekim42@gmail.com>
2021-11-08 12:49:46 +0300

    Update build script and solution file

--- build.cmd
+++ build.cmd
@@ -1,21 +1,21 @@
 @echo off
-Echo Building Microsoft.OpenApi 
+Echo Building Microsoft.OpenApi
 
-SET PROJ=%~dp0src\Microsoft.OpenApi\Microsoft.OpenApi.csproj 
+SET PROJ=%~dp0src\Microsoft.OpenApi\Microsoft.OpenApi.csproj
 dotnet msbuild %PROJ% /t:restore /p:Configuration=Release
 dotnet msbuild %PROJ% /t:build /p:Configuration=Release
 dotnet msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts
 
 Echo Building Microsoft.OpenApi.Readers
 
-SET PROJ=%~dp0src\Microsoft.OpenApi.Readers\Microsoft.OpenApi.Readers.csproj 
+SET PROJ=%~dp0src\Microsoft.OpenApi.Readers\Microsoft.OpenApi.Readers.csproj
 dotnet msbuild %PROJ% /t:restore /p:Configuration=Release
 dotnet msbuild %PROJ% /t:build /p:Configuration=Release
 dotnet msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts
 
-Echo Building Microsoft.OpenApi.Tool
+Echo Building Microsoft.Hidi
 
-SET PROJ=%~dp0src\Microsoft.OpenApi.Tool\Microsoft.OpenApi.Tool.csproj 
+SET PROJ=%~dp0src\Microsoft.Hidi\Microsoft.Hidi.csproj
 dotnet msbuild %PROJ% /t:restore /p:Configuration=Release
 dotnet msbuild %PROJ% /t:build /p:Configuration=Release
 dotnet msbuild %PROJ% /t:pack /p:Configuration=Release;PackageOutputPath=%~dp0artifacts
