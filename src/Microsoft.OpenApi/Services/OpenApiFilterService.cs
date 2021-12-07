494e59cdbef3ea3e091a770c138f41dbd2f4c78f
Maggie Kimani <maggiekim42@gmail.com>
2021-12-07 12:43:54 +0300

    Add necessary usings

--- src/Microsoft.OpenApi/Services/OpenApiFilterService.cs
+++ src/Microsoft.OpenApi/Services/OpenApiFilterService.cs
@@ -1,9 +1,11 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Microsoft.OpenApi.Models;
 
