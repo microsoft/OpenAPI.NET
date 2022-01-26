7ec682b656eda14f99a56d499f78e981ce9a218a
Maggie Kimani <maggiekim42@gmail.com>
2022-01-26 10:38:17 +0300

    Add csdl conversion tests

---
+++ test/Microsoft.OpenApi.Tests/Services/OpenApiCSDLConversionTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
+using System.IO;
+using Microsoft.OpenApi.Hidi;
+using Microsoft.OpenApi.Services;
+using Xunit;
+
+namespace Microsoft.OpenApi.Tests.Services
+{
+    public class OpenApiCSDLConversionTests
+    {
+        [Fact]
+        public void ReturnConvertedCSDLFile()
+        {
+            // Arrange
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UtilityFiles\\Todo.xml");
+            var fileInput = new FileInfo(filePath);
+            var csdlStream = fileInput.OpenRead();
+
+            // Act
+            var openApiDoc = OpenApiService.ConvertCsdlToOpenApi(csdlStream);
+            var expectedPathCount = 5;
+
+            // Assert
+            Assert.NotNull(openApiDoc);
+            Assert.NotEmpty(openApiDoc.Paths);
+            Assert.Equal(openApiDoc.Paths.Count, expectedPathCount);
+        }
+        
+        [Theory]
+        [InlineData("Todos.Todo.UpdateTodo",null, 1)]
+        [InlineData("Todos.Todo.ListTodo",null, 1)]
+        [InlineData(null, "Todos.Todo", 4)]
+        public void ReturnFilteredOpenApiDocumentBasedOnOperationIdsAndInputCsdlDocument(string operationIds, string tags, int expectedPathCount)
+        {
+            // Arrange
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UtilityFiles\\Todo.xml");
+            var fileInput = new FileInfo(filePath);
+            var csdlStream = fileInput.OpenRead();
+
+            // Act
+            var openApiDoc = OpenApiService.ConvertCsdlToOpenApi(csdlStream);
+            var predicate = OpenApiFilterService.CreatePredicate(operationIds, tags);
+            var subsetOpenApiDocument = OpenApiFilterService.CreateFilteredDocument(openApiDoc, predicate);
+
+            // Assert
+            Assert.NotNull(subsetOpenApiDocument);
+            Assert.NotEmpty(subsetOpenApiDocument.Paths);
+            Assert.Equal(expectedPathCount, subsetOpenApiDocument.Paths.Count);
+        }
+    }
+}
