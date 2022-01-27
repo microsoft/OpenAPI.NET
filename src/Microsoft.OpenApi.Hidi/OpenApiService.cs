526aa2928b6929632d27f81b1ca83fe73df62110
Maggie Kimani <maggiekim42@gmail.com>
2022-01-27 19:33:57 +0300

    Refactor param to be implicit

--- src/Microsoft.OpenApi.Hidi/OpenApiService.cs
+++ src/Microsoft.OpenApi.Hidi/OpenApiService.cs
@@ -115,10 +115,10 @@ namespace Microsoft.OpenApi.Hidi
             textWriter.Flush();
         }
 
-        private static Stream GetStream(string openapi)
+        private static Stream GetStream(string input)
         {
             Stream stream;
-            if (openapi.StartsWith("http"))
+            if (input.StartsWith("http"))
             {
                 var httpClient = new HttpClient(new HttpClientHandler()
                 {
@@ -127,11 +127,11 @@ namespace Microsoft.OpenApi.Hidi
                 {
                     DefaultRequestVersion = HttpVersion.Version20
                 };
-                stream = httpClient.GetStreamAsync(openapi).Result;
+                stream = httpClient.GetStreamAsync(input).Result;
             }
             else
             {
-                var fileInput = new FileInfo(openapi);
+                var fileInput = new FileInfo(input);
                 stream = fileInput.OpenRead();
             }
 
