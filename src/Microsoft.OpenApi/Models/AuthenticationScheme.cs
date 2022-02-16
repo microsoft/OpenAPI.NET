7b2821e8668b19c8dc34593b505cf1b2597f49e7
Maggie Kimani <maggiekim42@gmail.com>
2022-02-16 22:36:47 +0300

    Add enums to cater for multiple scheme and bearer formats

---
+++ src/Microsoft.OpenApi/Models/AuthenticationScheme.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. 
+
+using Microsoft.OpenApi.Attributes;
+
+namespace Microsoft.OpenApi.Models
+{
+    /// <summary>
+    /// The type of HTTP authentication schemes
+    /// </summary>
+    public enum AuthenticationScheme
+    {
+        /// <summary>
+        /// Use basic HTTP authentication schemes
+        /// </summary>
+        [Display("basic")] Basic,
+
+        /// <summary>
+        /// Use bearer Authentication scheme
+        /// </summary>
+        [Display("bearer")] Bearer,
+
+        /// <summary>
+        /// Use OpenIdConnectUrl
+        /// </summary>
+        [Display("openIdConnectUrl")] OpenIdConnectUrl
+
+        
+    }
+}
