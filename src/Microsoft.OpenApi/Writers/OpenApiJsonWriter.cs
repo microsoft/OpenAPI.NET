11c346692fff56d10e6ec76536452e60fa494100
Maggie Kimani <maggiekim42@gmail.com>
2022-04-14 13:57:27 +0300

    Add check to prevent null reference exceptions when settings are null

--- src/Microsoft.OpenApi/Writers/OpenApiJsonWriter.cs
+++ src/Microsoft.OpenApi/Writers/OpenApiJsonWriter.cs
@@ -35,7 +35,10 @@ namespace Microsoft.OpenApi.Writers
         /// <param name="settings">Settings for controlling how the OpenAPI document will be written out.</param>
         public OpenApiJsonWriter(TextWriter textWriter, OpenApiWriterSettings settings) : base(textWriter, settings)
         {
-            _produceTerseOutput = settings.Terse;
+            if (settings != null)
+            {
+                _produceTerseOutput = settings.Terse;
+            }
         }
 
         /// <summary>
