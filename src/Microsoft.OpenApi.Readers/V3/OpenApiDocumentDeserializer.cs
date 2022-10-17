43e165cc06e601faf9b0402c5d35d7d27d71b2f7
Maggie Kimani <maggiekim42@gmail.com>
2022-10-17 13:22:29 +0300

    Clean up project references

--- src/Microsoft.OpenApi.Readers/V3/OpenApiDocumentDeserializer.cs
+++ src/Microsoft.OpenApi.Readers/V3/OpenApiDocumentDeserializer.cs
@@ -1,10 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. 
 
-using System.Collections.Generic;
-using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Extensions;
-using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Readers.ParseNodes;
 
