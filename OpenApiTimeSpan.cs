3320c156978c3f00dfe310be26768c3799e55812
isekene <alo.hallmagi@hotmail.com>
2022-11-17 13:39:12 +0200

    Add files via upload
    added timespan type

---
+++ OpenApiTimeSpan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.OpenApi.Any
+{
+    /// <summary>
+    /// Open API TimeSpan
+    /// </summary>
+    public class OpenApiTimeSpan : OpenApiPrimitive<TimeSpan>
+    {
+        /// <summary>
+        /// Initializes the <see cref="OpenApiTimeSpan"/> class.
+        /// </summary>
+        public OpenApiTimeSpan(TimeSpan value)
+            : base(value)
+        {
+        }
+
+        /// <summary>
+        /// Primitive type this object represents.
+        /// </summary>
+        public override PrimitiveType PrimitiveType { get; } = PrimitiveType.TimeSpan;
+    }
+}
