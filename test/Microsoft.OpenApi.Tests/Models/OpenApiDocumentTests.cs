a6b5d2095ed6a7ad91c86100a41635536112de34
Maggie Kimani <maggiekim42@gmail.com>
2022-11-28 11:20:50 +0300

    Remove static modifier as it creates static dependency of the document instance when running tests in parallel causing one to fail

--- test/Microsoft.OpenApi.Tests/Models/OpenApiDocumentTests.cs
+++ test/Microsoft.OpenApi.Tests/Models/OpenApiDocumentTests.cs
@@ -1,10 +1,11 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. 
 
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.OpenApi.Extensions;
@@ -598,7 +599,7 @@ namespace Microsoft.OpenApi.Tests.Models
 
         public static OpenApiSchema ErrorModelSchema = AdvancedComponents.Schemas["errorModel"];
 
-        public static OpenApiDocument AdvancedDocument = new OpenApiDocument
+        public OpenApiDocument AdvancedDocument = new OpenApiDocument
         {
             Info = new OpenApiInfo
             {
