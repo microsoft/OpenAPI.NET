8a93c141697f83b8e998ae9be78651eac26abe27
Irvine Sunday <irochand@microsoft.com>
2023-04-03 17:08:18 +0300

    Use DictionaryCloneHelper

--- src/Microsoft.OpenApi/Models/OpenApiDocument.cs
+++ src/Microsoft.OpenApi/Models/OpenApiDocument.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using Microsoft.OpenApi.Exceptions;
+using Microsoft.OpenApi.Helpers;
 using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Services;
 using Microsoft.OpenApi.Writers;
@@ -88,7 +89,7 @@ namespace Microsoft.OpenApi.Models
             SecurityRequirements = document?.SecurityRequirements != null ? new List<OpenApiSecurityRequirement>(document.SecurityRequirements) : null;
             Tags = document?.Tags != null ? new List<OpenApiTag>(document.Tags) : null;
             ExternalDocs = document?.ExternalDocs != null ? new(document?.ExternalDocs) : null;
-            Extensions = document?.Extensions != null ? new Dictionary<string, IOpenApiExtension>(document.Extensions) : null;
+            Extensions = document?.Extensions != null ? DictionaryCloneHelper.Clone(document.Extensions) : null;
         }
 
         /// <summary>
