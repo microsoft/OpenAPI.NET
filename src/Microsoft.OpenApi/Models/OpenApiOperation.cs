6eac7c0211f2b01331d90018475bed9d5966b09d
Maggie Kimani <maggiekim42@gmail.com>
2023-04-17 16:24:29 +0300

    Remove unnecesary using; bump lib versions

--- src/Microsoft.OpenApi/Models/OpenApiOperation.cs
+++ src/Microsoft.OpenApi/Models/OpenApiOperation.cs
@@ -4,8 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Helpers;
 using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Writers;
 
