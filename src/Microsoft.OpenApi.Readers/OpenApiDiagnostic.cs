10806b71a55a5686592ebc715c0be28b666b1fcc
Elize van der Riet <elize.van.der.riet@mendix.com>
2023-05-11 14:01:52 +0200

    ISSUE-1131: Added diagnostic reporting recursive concatenation of diagnostic reports from referenced files to the main file's diagnostic report

--- src/Microsoft.OpenApi.Readers/OpenApiDiagnostic.cs
+++ src/Microsoft.OpenApi.Readers/OpenApiDiagnostic.cs
@@ -26,5 +26,61 @@ namespace Microsoft.OpenApi.Readers
         /// Open API specification version of the document parsed.
         /// </summary>
         public OpenApiSpecVersion SpecificationVersion { get; set; }
+
+        /// <summary>
+        /// Append another set of diagnostic Errors and Warnings to this one, this may be appended from another external
+        /// document's parsing and we want to indicate which file it originated from.
+        /// </summary>
+        /// <param name="diagnosticToAdd"></param>
+        /// <param name="fileNameToAdd"></param>
+        public void AppendDiagnostic(OpenApiDiagnostic diagnosticToAdd, string fileNameToAdd)
+        {
+            foreach (var err in diagnosticToAdd.Errors)
+            {
+                string errMsgWithFileName;
+                if (!(string.IsNullOrEmpty(fileNameToAdd) || string.IsNullOrWhiteSpace(fileNameToAdd)))
+                {
+                    errMsgWithFileName = $"[File: {fileNameToAdd}] {err.Message}";
+                }
+                else
+                {
+                    errMsgWithFileName = err.Message;
+                }
+                Errors.Add(new OpenApiError(err.Pointer, errMsgWithFileName));
+            }
+            foreach (var warn in diagnosticToAdd.Warnings)
+            {
+                string warnMsgWithFileName;
+                if (!(string.IsNullOrEmpty(fileNameToAdd) || string.IsNullOrWhiteSpace(fileNameToAdd)))
+                {
+                    warnMsgWithFileName = $"[File: {fileNameToAdd}] {warn.Message}";
+                }
+                else
+                {
+                    warnMsgWithFileName = warn.Message;
+                }
+                Errors.Add(new OpenApiError(warn.Pointer, warnMsgWithFileName));
+            }
+        }
+    }
+}
+
+/// <summary>
+/// Extension class for IList to add the Method "AddRange" used above
+/// </summary>
+public static class IDiagnosticExtensions
+{
+    /// <summary>
+    /// Extension method for IList so that another list can be added to the current list.
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="enumerable"></param>
+    /// <typeparam name="T"></typeparam>
+    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> enumerable)
+    {
+        foreach (var cur in enumerable)
+        {
+            collection.Add(cur);
+        }
     }
 }
--- test/Microsoft.OpenApi.Readers.Tests/OpenApiReaderTests/OpenApiDiagnosticTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/OpenApiReaderTests/OpenApiDiagnosticTests.cs
@@ -1,8 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. 
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
 using FluentAssertions;
+using Microsoft.OpenApi.Exceptions;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers.Tests.OpenApiWorkspaceTests;
 using Xunit;
+using Microsoft.OpenApi.Readers.Interface;
+using System.IO;
 
 namespace Microsoft.OpenApi.Readers.Tests.OpenApiReaderTests
 {
@@ -31,6 +39,46 @@ namespace Microsoft.OpenApi.Readers.Tests.OpenApiReaderTests
                 diagnostic.Should().NotBeNull();
                 diagnostic.SpecificationVersion.Should().Be(OpenApiSpecVersion.OpenApi3_0);
             }
+        }
+
+        [Fact]
+        public async Task DiagnosticReportMergedForExternalReference()
+        {
+            // Create a reader that will resolve all references
+            var reader = new OpenApiStreamReader(new OpenApiReaderSettings()
+            {
+                LoadExternalRefs = true,
+                CustomExternalLoader = new ResourceLoader(),
+                BaseUrl = new Uri("fie://c:\\")
+            });
+
+            ReadResult result;
+            using (var stream = Resources.GetStream("OpenApiReaderTests/Samples/OpenApiDiagnosticReportMerged/TodoMain.yaml"))
+            {
+                result = await reader.ReadAsync(stream);
+            }
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.OpenApiDocument.Workspace);
+            Assert.True(result.OpenApiDocument.Workspace.Contains("TodoReference.yaml"));
+            result.OpenApiDiagnostic.Errors.Should().BeEquivalentTo(new List<OpenApiError> {
+                new OpenApiError( new OpenApiException("[File: ./TodoReference.yaml] Invalid Reference identifier 'object-not-existing'.")) });
+
+        }
+    }
+
+    public class ResourceLoader : IStreamLoader
+    {
+        public Stream Load(Uri uri)
+        {
+            return null;
+        }
+
+        public Task<Stream> LoadAsync(Uri uri)
+        {
+            var path = new Uri(new Uri("http://example.org/OpenApiReaderTests/Samples/OpenApiDiagnosticReportMerged/"), uri).AbsolutePath;
+            path = path.Substring(1); // remove leading slash
+            return Task.FromResult(Resources.GetStream(path));
         }
     }
 }
