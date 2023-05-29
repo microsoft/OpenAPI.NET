7e253558df807252e0f7f789fd28e8c183e76d30
Irvine Sunday <irochand@microsoft.com>
2023-05-29 16:01:11 +0300

    Adds missing using

--- src/Microsoft.OpenApi/Models/OpenApiComponents.cs
+++ src/Microsoft.OpenApi/Models/OpenApiComponents.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. 
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.OpenApi.Interfaces;
