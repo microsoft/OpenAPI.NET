799102290c701b5a2f8b66cae2644f03ec96040d
Maggie Kimani <maggiekim42@gmail.com>
2023-09-13 15:59:35 +0300

    Remove test due to alternate reference resolution logic

--- test/Microsoft.OpenApi.Readers.Tests/V2Tests/OpenApiDocumentTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/V2Tests/OpenApiDocumentTests.cs
@@ -18,34 +18,6 @@ namespace Microsoft.OpenApi.Readers.Tests.V2Tests
         private const string SampleFolderPath = "V2Tests/Samples/";
 
         [Fact]
-        public void ShouldThrowWhenReferenceDoesNotExist()
-        {
-            var input = @"
-swagger: 2.0
-info: 
-  title: test
-  version: 1.0.0
-paths: 
-  '/':
-    get:
-      produces: ['application/json']
-      responses:
-        '200':
-          description: ok
-          schema:
-            $ref: '#/definitions/doesnotexist'
-";
-
-            var reader = new OpenApiStringReader();
-
-            var doc = reader.Read(input, out var diagnostic);
-
-            diagnostic.Errors.Should().BeEquivalentTo(new List<OpenApiError> {
-                new OpenApiError( new OpenApiException("Invalid Reference identifier 'doesnotexist'.")) });
-            doc.Should().NotBeNull();
-        }
-
-        [Fact]
         public void ShouldParseProducesInAnyOrder()
         {
             using (var stream = Resources.GetStream(Path.Combine(SampleFolderPath, "twoResponses.json")))
