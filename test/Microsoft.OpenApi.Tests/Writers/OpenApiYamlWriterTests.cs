8f93584a8b448eebb473312bcc29dd680b686b13
Simon Cropp <simon.cropp@gmail.com>
2023-10-02 21:17:21 +1100

    use is operator instead of reflection

--- test/Microsoft.OpenApi.Tests/Writers/OpenApiYamlWriterTests.cs
+++ test/Microsoft.OpenApi.Tests/Writers/OpenApiYamlWriterTests.cs
@@ -276,10 +276,10 @@ property4: value4"
 
                 writer.WriteEndObject();
             }
-            else if (typeof(IEnumerable).IsAssignableFrom(value.GetType()))
+            else if (value is IEnumerable enumerable)
             {
                 writer.WriteStartArray();
-                foreach (var elementValue in (IEnumerable)value)
+                foreach (var elementValue in enumerable)
                 {
                     WriteValueRecursive(writer, elementValue);
                 }
