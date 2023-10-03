9901516a285afd10ba0f13b378f38acdc11a5dd4
Simon Cropp <simon.cropp@gmail.com>
2023-10-03 20:23:27 +1100

    make indents consistent

--- src/Microsoft.OpenApi/MicrosoftExtensions/OpenApiPrimaryErrorMessageExtension.cs
+++ src/Microsoft.OpenApi/MicrosoftExtensions/OpenApiPrimaryErrorMessageExtension.cs
@@ -15,31 +15,34 @@ namespace Microsoft.OpenApi.MicrosoftExtensions;
 /// </summary>
 public class OpenApiPrimaryErrorMessageExtension : IOpenApiExtension
 {
-	/// <summary>
-	/// Name of the extension as used in the description.
-	/// </summary>
-	public static string Name => "x-ms-primary-error-message";
-	/// <inheritdoc />
+    /// <summary>
+    /// Name of the extension as used in the description.
+    /// </summary>
+    public static string Name => "x-ms-primary-error-message";
+
+    /// <inheritdoc />
     public void Write(IOpenApiWriter writer, OpenApiSpecVersion specVersion)
     {
-        if(writer is null) throw new ArgumentNullException(nameof(writer));
-		writer.WriteValue(IsPrimaryErrorMessage);
+        if (writer is null) throw new ArgumentNullException(nameof(writer));
+        writer.WriteValue(IsPrimaryErrorMessage);
     }
-	/// <summary>
-	/// Whether this property is the primary error message to use on error types.
-	/// </summary>
-	public bool IsPrimaryErrorMessage { get; set; }
-	/// <summary>
+
+    /// <summary>
+    /// Whether this property is the primary error message to use on error types.
+    /// </summary>
+    public bool IsPrimaryErrorMessage { get; set; }
+
+    /// <summary>
     /// Parses the <see cref="IOpenApiAny"/> to <see cref="OpenApiPrimaryErrorMessageExtension"/>.
     /// </summary>
     /// <param name="source">The source object.</param>
     /// <returns>The <see cref="OpenApiPrimaryErrorMessageExtension"/>.</returns>
-	public static OpenApiPrimaryErrorMessageExtension Parse(IOpenApiAny source)
-	{
-		if (source is not OpenApiBoolean rawObject) throw new ArgumentOutOfRangeException(nameof(source));
-		return new OpenApiPrimaryErrorMessageExtension
+    public static OpenApiPrimaryErrorMessageExtension Parse(IOpenApiAny source)
+    {
+        if (source is not OpenApiBoolean rawObject) throw new ArgumentOutOfRangeException(nameof(source));
+        return new OpenApiPrimaryErrorMessageExtension
         {
-			IsPrimaryErrorMessage = rawObject.Value
-		};
-	}
+            IsPrimaryErrorMessage = rawObject.Value
+        };
+    }
 }
