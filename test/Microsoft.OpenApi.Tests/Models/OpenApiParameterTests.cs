f5037576035db8296c1e1b46fe3e414990d3df45
Maggiekimani1 <maggiekim42@gmail.com>
2023-12-05 16:25:57 +0300

    Clean up code; update tests and public API

--- test/Microsoft.OpenApi.Tests/Models/OpenApiParameterTests.cs
+++ test/Microsoft.OpenApi.Tests/Models/OpenApiParameterTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 using System.Collections.Generic;
@@ -296,7 +296,13 @@ schema:
                   "name": "name1",
                   "description": "description1",
                   "required": true,
-                  "format": "double"
+                  "format": "double",
+                  "x-examples": {
+                    "test": {
+                      "summary": "summary3",
+                      "description": "description3"
+                    }
+                  }
                 }
                 """;
 
