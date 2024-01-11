ad85b8711d7cd4eb1f1470d10862fad4211275af
Vincent Biret <vibiret@microsoft.com>
2024-01-11 09:45:23 -0500

    - adds missing using

--- src/Microsoft.OpenApi.Readers/V2/OpenApiResponseDeserializer.cs
+++ src/Microsoft.OpenApi.Readers/V2/OpenApiResponseDeserializer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using Json.Schema;
 using Microsoft.OpenApi.Extensions;
