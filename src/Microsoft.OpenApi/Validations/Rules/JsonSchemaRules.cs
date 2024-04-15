b90aa0322fa33f8af97ca7109ae9bc4a2d337264
Maggiekimani1 <maggiekim42@gmail.com>
2024-04-15 18:16:59 +0300

    Remove unnecessary using

--- src/Microsoft.OpenApi/Validations/Rules/JsonSchemaRules.cs
+++ src/Microsoft.OpenApi/Validations/Rules/JsonSchemaRules.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Json.Schema;
 using Json.Schema.OpenApi;
-using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Properties;
 
