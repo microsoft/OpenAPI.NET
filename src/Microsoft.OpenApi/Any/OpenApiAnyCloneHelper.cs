d8eda7aca828415f253aa6f0ac3780f303719f51
Safia Abdalla <safia@safia.rocks>
2024-07-03 18:05:02 -0700

    Make library trim-compatible

--- src/Microsoft.OpenApi/Any/OpenApiAnyCloneHelper.cs
+++ src/Microsoft.OpenApi/Any/OpenApiAnyCloneHelper.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-using System.Reflection;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.OpenApi.Any
 {
@@ -15,17 +15,16 @@ namespace Microsoft.OpenApi.Any
         /// </summary>
         /// <param name="obj">The object instance.</param>
         /// <returns>A clone copy or the object itself.</returns>
-        public static IOpenApiAny CloneFromCopyConstructor(IOpenApiAny obj)
+        public static IOpenApiAny CloneFromCopyConstructor<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(T obj) where T : IOpenApiAny
         {
             if (obj != null)
             {
-                var t = obj.GetType();
-                foreach (var ci in t.GetConstructors())
+                foreach (var ci in typeof(T).GetConstructors())
                 {
                     var pi = ci.GetParameters();
-                    if (pi.Length == 1 && pi[0].ParameterType == t)
+                    if (pi.Length == 1 && pi[0].ParameterType == typeof(T))
                     {
-                        return (IOpenApiAny)ci.Invoke(new object[] { obj });
+                        return (IOpenApiAny)ci.Invoke([obj]);
                     }
                 }
             }
--- src/Microsoft.OpenApi/Extensions/EnumExtensions.cs
+++ src/Microsoft.OpenApi/Extensions/EnumExtensions.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Reflection;
 using Microsoft.OpenApi.Attributes;
+using Microsoft.OpenApi.Models;
 
 namespace Microsoft.OpenApi.Extensions
 {
@@ -42,5 +43,107 @@ namespace Microsoft.OpenApi.Extensions
             var attribute = enumValue.GetAttributeOfType<DisplayAttribute>();
             return attribute == null ? enumValue.ToString() : attribute.Name;
         }
+
+        /// <summary>
+        /// Gets the enum display name for <see typeparamref="T"/> without the use of reflection. 
+        /// </summary>
+        /// <typeparam name="T">The type of the enum value.</typeparam>
+        /// <param name="enumValue">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        public static string GetDisplayName<T>(this T enumValue) where T : Enum
+        {
+            return enumValue switch
+            {
+                ParameterStyle parameterStyle => parameterStyle.GetDisplayName(),
+                ParameterLocation parameterLocation => parameterLocation.GetDisplayName(),
+                ReferenceType referenceType => referenceType.GetDisplayName(),
+                OperationType operationType => operationType.GetDisplayName(),
+                SecuritySchemeType securitySchemeType => securitySchemeType.GetDisplayName(),
+                _ => enumValue.ToString()
+            };
+        }
+
+        /// <summary>
+        /// Gets the enum display for name <see cref="ParameterStyle" /> without the use of reflection.
+        /// </summary>
+        /// <param name="parameterStyle">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        internal static string GetDisplayName(this ParameterStyle parameterStyle) => parameterStyle switch
+            {
+                ParameterStyle.Matrix => "matrix",
+                ParameterStyle.Label => "label",
+                ParameterStyle.Form => "form",
+                ParameterStyle.Simple => "simple",
+                ParameterStyle.SpaceDelimited => "spaceDelimited",
+                ParameterStyle.PipeDelimited => "pipeDelimited",
+                ParameterStyle.DeepObject => "deepObject",
+                _ => parameterStyle.ToString()
+            };
+
+        /// <summary>
+        /// Gets the enum display for name <see cref="ParameterLocation" /> without the use of reflection. 
+        /// </summary>
+        /// <param name="parameterLocation">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        internal static string GetDisplayName(this ParameterLocation parameterLocation) => parameterLocation switch
+            {
+                ParameterLocation.Query => "query",
+                ParameterLocation.Header => "header",
+                ParameterLocation.Path => "path",
+                ParameterLocation.Cookie => "cookie",
+                _ => parameterLocation.ToString()
+            };
+
+        /// <summary>
+        /// Gets the enum display for name <see cref="ReferenceType" /> without the use of reflection. 
+        /// </summary>
+        /// <param name="referenceType">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        internal static string GetDisplayName(this ReferenceType referenceType) => referenceType switch
+            {
+                ReferenceType.Schema => "schemas",
+                ReferenceType.Response => "responses",
+                ReferenceType.Parameter => "parameters",
+                ReferenceType.Example => "examples",
+                ReferenceType.RequestBody => "requestBodies",
+                ReferenceType.Header => "headers",
+                ReferenceType.SecurityScheme => "securitySchemes",
+                ReferenceType.Link => "links",
+                ReferenceType.Callback => "callbacks",
+                ReferenceType.Tag => "tags",
+                _ => referenceType.ToString()
+            };
+
+        /// <summary>
+        /// Gets the enum display for name <see cref="OperationType" /> without the use of reflection. 
+        /// </summary>
+        /// <param name="operationType">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        internal static string GetDisplayName(this OperationType operationType) => operationType switch
+            {
+                OperationType.Get => "get",
+                OperationType.Put => "put",
+                OperationType.Post => "post",
+                OperationType.Delete => "delete",
+                OperationType.Options => "options",
+                OperationType.Head => "head",
+                OperationType.Patch => "patch",
+                OperationType.Trace => "trace",
+                _ => operationType.ToString()
+            };
+
+        /// <summary>
+        /// Gets the enum display for name <see cref="SecuritySchemeType" /> without the use of reflection. 
+        /// </summary>
+        /// <param name="securitySchemeType">The enum value.</param>
+        /// <returns>The display string to use.</returns>
+        internal static string GetDisplayName(this SecuritySchemeType securitySchemeType) => securitySchemeType switch
+            {
+                SecuritySchemeType.ApiKey => "apiKey",
+                SecuritySchemeType.Http => "http",
+                SecuritySchemeType.OAuth2 => "oauth2",
+                SecuritySchemeType.OpenIdConnect => "openIdConnect",
+                _ => securitySchemeType.ToString()
+            };
     }
 }
