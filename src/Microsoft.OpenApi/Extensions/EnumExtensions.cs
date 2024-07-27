1f815a2c11f8a79023a84ceceb0e08ca79927808
Mahdi Golestan <Mahdi Golestan>
2024-07-27 14:54:11 +0330

    Use ConcurrentDictionary For Improving Get Enum Name

--- src/Microsoft.OpenApi/Extensions/EnumExtensions.cs
+++ src/Microsoft.OpenApi/Extensions/EnumExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 using System;
+using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
@@ -14,6 +15,9 @@ namespace Microsoft.OpenApi.Extensions
     /// </summary>
     public static class EnumExtensions
     {
+        // Cache to store display names of enum values
+        private static readonly ConcurrentDictionary<Enum, string> DisplayNameCache = new();
+
         /// <summary>
         /// Gets an attribute on an enum field value.
         /// </summary>
@@ -26,7 +30,13 @@ namespace Microsoft.OpenApi.Extensions
         public static T GetAttributeOfType<T>(this Enum enumValue) where T : Attribute
         {
             var type = enumValue.GetType();
+            // Use GetField to get the field info for the enum value
             var memInfo = type.GetField(enumValue.ToString(), BindingFlags.Public | BindingFlags.Static);
+
+            if (memInfo == null)
+                return null;
+
+            // Retrieve the custom attributes of type T
             var attributes = memInfo.GetCustomAttributes<T>(false);
             return attributes.FirstOrDefault();
         }
@@ -36,13 +46,20 @@ namespace Microsoft.OpenApi.Extensions
         /// </summary>
         /// <param name="enumValue">The enum value.</param>
         /// <returns>
-        /// Use <see cref="DisplayAttribute"/> if exists.
+        /// Use <see cref="DisplayAttribute"/> if it exists.
         /// Otherwise, use the standard string representation.
         /// </returns>
         public static string GetDisplayName(this Enum enumValue)
         {
-            var attribute = enumValue.GetAttributeOfType<DisplayAttribute>();
-            return attribute == null ? enumValue.ToString() : attribute.Name;
+            // Retrieve the display name from the cache if it exists
+            return DisplayNameCache.GetOrAdd(enumValue, e =>
+            {
+                // Get the DisplayAttribute
+                var attribute = e.GetAttributeOfType<DisplayAttribute>();
+
+                // Return the DisplayAttribute name if it exists, otherwise return the enum's string representation
+                return attribute == null ? e.ToString() : attribute.Name;
+            });
         }
     }
 }
