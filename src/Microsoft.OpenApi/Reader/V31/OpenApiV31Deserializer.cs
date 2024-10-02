e8275986af7ac97f0ba4b5b33ea18eb9a5cc6a7d
Maggiekimani1 <maggiekim42@gmail.com>
2024-10-02 13:27:41 +0300

    Clean up logic for determining whether a locator or identifier references an external resource

--- src/Microsoft.OpenApi/Reader/V31/OpenApiV31Deserializer.cs
+++ src/Microsoft.OpenApi/Reader/V31/OpenApiV31Deserializer.cs
@@ -157,9 +157,11 @@ namespace Microsoft.OpenApi.Reader.V31
             string refId = !pointer.Contains('#') ? pointer : refSegments.Last();
 
             var isExternalResource = !refSegments.First().StartsWith("#");
-            string externalResource = isExternalResource
-                ? $"{refSegments.First()}/{refSegments[1].TrimEnd('#')}"
-                : null;
+            string externalResource = null;
+            if (isExternalResource && pointer.Contains('#'))
+            {
+                externalResource = $"{refSegments.First()}/{refSegments[1].TrimEnd('#')}";
+            }
 
             return (refId, externalResource);
         }
