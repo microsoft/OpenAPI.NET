8902656681487c22451c08cdc3b4e4aef41db9be
Vincent Biret <vibiret@microsoft.com>
2024-12-23 12:38:41 -0500

    chore: adds test for deconstructor
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ test/Microsoft.OpenApi.Tests/Reader/ReadResultTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Microsoft.OpenApi.Reader;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.OpenApi.Tests.Reader;
+
+public class ReadResultTests
+{
+    [Fact]
+    public void Deconstructs()
+    {
+        var readResult = new ReadResult()
+        {
+            Document = new OpenApiDocument(),
+            Diagnostic = new OpenApiDiagnostic()
+        };
+        var (document, diagnostic) = readResult;
+        Assert.Equal(readResult.Document, document);
+        Assert.Equal(readResult.Diagnostic, diagnostic);
+    }
+}
