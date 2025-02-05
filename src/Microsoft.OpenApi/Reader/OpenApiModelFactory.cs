711b3d17a07c23930e9ae5ca5b2d815701e2516d
Vincent Biret <vibiret@microsoft.com>
2025-02-05 07:51:12 -0500

    Update src/Microsoft.OpenApi/Reader/OpenApiModelFactory.cs

--- src/Microsoft.OpenApi/Reader/OpenApiModelFactory.cs
+++ src/Microsoft.OpenApi/Reader/OpenApiModelFactory.cs
@@ -298,11 +298,8 @@ namespace Microsoft.OpenApi.Reader
                     var response = await _httpClient.GetAsync(url, token).ConfigureAwait(false);
                     var mediaType = response.Content.Headers.ContentType.MediaType;
                     var contentType = mediaType.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
-                    format = contentType.Split('/').LastOrDefault();
-                    if (!string.IsNullOrEmpty(format) && format.Contains('-'))
-                    {
-                        format = format.Split('-').LastOrDefault(); // for non-standard MIME types e.g. text/x-yaml used in older libs or apps
-                    }
+                    format = contentType.Split('/').Last().Split('+').Last().Split('-').Last();
+                    // for non-standard MIME types e.g. text/x-yaml used in older libs or apps
 #if NETSTANDARD2_0
                     stream = await response.Content.ReadAsStreamAsync();
 #else
