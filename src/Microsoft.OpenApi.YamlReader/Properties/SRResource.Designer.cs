1a689bd7c7e0aa45aee2b3c4ed56736de0ecd7dd
Maggiekimani1 <maggiekim42@gmail.com>
2025-02-25 18:23:36 +0300

    fix: clean up project references

--- src/Microsoft.OpenApi.YamlReader/Properties/SRResource.Designer.cs
+++ src/Microsoft.OpenApi.YamlReader/Properties/SRResource.Designer.cs
@@ -39,7 +39,7 @@ namespace Microsoft.OpenApi.YamlReader.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.OpenApi.Readers.Properties.SRResource", typeof(SRResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.OpenApi.YamlReader.Properties.SRResource", typeof(SRResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
