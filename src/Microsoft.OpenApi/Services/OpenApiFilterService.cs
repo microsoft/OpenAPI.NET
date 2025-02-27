ad59b9c793989c4b37e190c69c9c20b262ef31fb
Vincent Biret <vibiret@microsoft.com>
2025-02-27 11:00:02 -0500

    chore: bad merge
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/Microsoft.OpenApi/Services/OpenApiFilterService.cs
+++ src/Microsoft.OpenApi/Services/OpenApiFilterService.cs
@@ -80,7 +80,7 @@ namespace Microsoft.OpenApi.Services
                 },
 
                 Components = components,
-                SecurityRequirements = source?.Security,
+                Security = source?.Security,
                 Servers = source?.Servers
             };
 
