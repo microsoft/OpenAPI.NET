b7cb5f0fb5a6592322bb08bcde054a5eaa5ed2f7
Vincent Biret <vibiret@microsoft.com>
2025-03-28 14:20:27 -0400

    ci: performance project scaffolding

---
+++ performance/Benchmarks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace performance
+{
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void Scenario1()
+        {
+            // Implement your benchmark here
+        }
+
+        [Benchmark]
+        public void Scenario2()
+        {
+            // Implement your benchmark here
+        }
+    }
+}
