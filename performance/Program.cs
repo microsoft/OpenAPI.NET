475bfeb2b65774bfd61e008966e5a30301f32734
Vincent Biret <vibiret@microsoft.com>
2025-03-28 14:20:27 -0400

    ci: performance project scaffolding

---
+++ performance/Program.cs
@@ -0,0 +1,17 @@
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance;
+            var summary = BenchmarkRunner.Run<Benchmarks>(config, args);
+
+            // Use this to select benchmarks from the console:
+            // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+        }
+    }
+}
