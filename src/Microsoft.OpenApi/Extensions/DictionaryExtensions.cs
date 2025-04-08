37e130d45f196d1048dcdb435decc531611a5ab3
Michael Mutunga <michaelwamae28@gmail.com>
2025-04-08 16:50:53 +0300

    update dictionary extension

--- src/Microsoft.OpenApi/Extensions/DictionaryExtensions.cs
+++ src/Microsoft.OpenApi/Extensions/DictionaryExtensions.cs
@@ -12,34 +12,27 @@ namespace Microsoft.OpenApi.Extensions
         /// <summary>
         /// Returns a new dictionary with entries sorted by key using the default comparer.
         /// </summary>
-        public static IDictionary<TKey, TValue> Sort<TKey, TValue>(
+        public static SortedDictionary<TKey, TValue> Sort<TKey, TValue>(
             this IDictionary<TKey, TValue> source)
             where TKey : notnull
         {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
+            Utils.CheckArgumentNull(source);
 
-            return source
-                .OrderBy(kvp => kvp.Key)
-                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            return new SortedDictionary<TKey, TValue>(source);
         }
 
         /// <summary>
         /// Returns a new dictionary with entries sorted by key using a custom comparer.
         /// </summary>
-        public static IDictionary<TKey, TValue> Sort<TKey, TValue>(
+        public static SortedDictionary<TKey, TValue> Sort<TKey, TValue>(
             this IDictionary<TKey, TValue> source,
             IComparer<TKey> comparer)
             where TKey : notnull
         {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            if (comparer == null)
-                throw new ArgumentNullException(nameof(comparer));
+            Utils.CheckArgumentNull(source);
+            Utils.CheckArgumentNull(comparer);
 
-            return source
-                .OrderBy(kvp => kvp.Key, comparer)
-                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            return new SortedDictionary<TKey, TValue>(source, comparer);
         }
     }
 }
