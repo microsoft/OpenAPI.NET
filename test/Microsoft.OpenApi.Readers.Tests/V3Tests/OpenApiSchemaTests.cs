f7e637f0d1bbd77e695b4aa4cb58dbbb081df4a6
Maggiekimani1 <maggiekim42@gmail.com>
2025-04-17 18:45:33 +0300

    chore: use implicit operator

--- test/Microsoft.OpenApi.Readers.Tests/V3Tests/OpenApiSchemaTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/V3Tests/OpenApiSchemaTests.cs
@@ -70,12 +70,12 @@ namespace Microsoft.OpenApi.Readers.Tests.V3Tests
             // Assert
             Assert.Equivalent(new OpenApiDiagnostic(), diagnostic);
 
-            openApiAny.Should().BeEquivalentTo(new OpenApiAny(
+            openApiAny.Should().BeEquivalentTo((OpenApiAny)
                 new JsonObject
                 {
                     ["foo"] = "bar",
                     ["baz"] = new JsonArray() { 1, 2 }
-                }), options => options.IgnoringCyclicReferences());
+                }, options => options.IgnoringCyclicReferences());
         }
 
         [Fact]
