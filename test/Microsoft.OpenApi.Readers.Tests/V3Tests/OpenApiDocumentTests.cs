ad859ea0230ad7a7c71bae506c0b630c86ed1b2c
Maggiekimani1 <maggiekim42@gmail.com>
2025-05-19 11:38:32 +0300

    chore: add comment

--- test/Microsoft.OpenApi.Readers.Tests/V3Tests/OpenApiDocumentTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/V3Tests/OpenApiDocumentTests.cs
@@ -1270,7 +1270,7 @@ paths: {}
                 Description = "A reference to a pet"
             };
             document.AddComponent("PetReference", petSchemaReference);
-            document.AddComponent("Pet", petSchema);
+            document.AddComponent("Pet", petSchema); // does not add duplicate keys
             document.Paths.Add("/pets", new OpenApiPathItem
             {
                 Operations = new()
