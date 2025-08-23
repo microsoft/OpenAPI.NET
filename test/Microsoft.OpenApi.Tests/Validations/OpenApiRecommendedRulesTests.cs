92564f34a2a58227d79f11bd560aec9d453b6673
martincostello <martin@martincostello.com>
2025-08-23 10:13:56 +0100

    Test not part of default rules
    Address feedback.

--- test/Microsoft.OpenApi.Tests/Validations/OpenApiRecommendedRulesTests.cs
+++ test/Microsoft.OpenApi.Tests/Validations/OpenApiRecommendedRulesTests.cs
@@ -36,7 +36,7 @@ public static class OpenApiRecommendedRulesTests
             }
         });
 
-        document.Paths.Add("people", new OpenApiPathItem
+        document.Paths.Add("/people", new OpenApiPathItem
         {
             Operations = new Dictionary<HttpMethod, OpenApiOperation>()
             {
@@ -92,13 +92,13 @@ public static class OpenApiRecommendedRulesTests
         ruleSet.Add(typeof(OpenApiPaths), OpenApiRecommendedRules.GetOperationShouldNotHaveRequestBody);
 
         // Act
-        var errors = document.Validate(ruleSet);
-        var result = !errors.Any();
+        var warnings = document.Validate(ruleSet);
+        var result = !warnings.Any();
 
         // Assert
         Assert.True(result);
-        Assert.NotNull(errors);
-        Assert.Empty(errors);
+        Assert.NotNull(warnings);
+        Assert.Empty(warnings);
     }
 
     [Fact]
@@ -127,7 +127,7 @@ public static class OpenApiRecommendedRulesTests
             }
         });
 
-        document.Paths.Add("people", new OpenApiPathItem
+        document.Paths.Add("/people", new OpenApiPathItem
         {
             Operations = new Dictionary<HttpMethod, OpenApiOperation>()
             {
@@ -200,6 +200,105 @@ public static class OpenApiRecommendedRulesTests
         Assert.NotNull(warnings);
         var warning = Assert.Single(warnings);
         Assert.Equal("GET operations should not have a request body.", warning.Message);
-        Assert.Equal("#/paths/people/get/requestbody", warning.Pointer);
+        Assert.Equal("#/paths//people/get/requestBody", warning.Pointer);
+    }
+
+    [Fact]
+    public static void GetOperationWithRequestBodyIsValidUsingDefaultRuleSet()
+    {
+        // Arrange
+        var document = new OpenApiDocument
+        {
+            Components = new OpenApiComponents(),
+            Info = new OpenApiInfo
+            {
+                Title = "People Document",
+                Version = "1.0.0"
+            },
+            Paths = [],
+            Workspace = new()
+        };
+
+        document.AddComponent("Person", new OpenApiSchema
+        {
+            Type = JsonSchemaType.Object,
+            Properties = new Dictionary<string, IOpenApiSchema>()
+            {
+                ["name"] = new OpenApiSchema { Type = JsonSchemaType.String },
+                ["email"] = new OpenApiSchema { Type = JsonSchemaType.String, Format = "email" }
+            }
+        });
+
+        document.Paths.Add("/people", new OpenApiPathItem
+        {
+            Operations = new Dictionary<HttpMethod, OpenApiOperation>()
+            {
+                [HttpMethod.Get] = new OpenApiOperation
+                {
+                    RequestBody = new OpenApiRequestBody
+                    {
+                        Content = new Dictionary<string, OpenApiMediaType>()
+                        {
+                            ["application/json"] = new OpenApiMediaType
+                            {
+                                Schema = new OpenApiSchemaReference("Person", document),
+                            }
+                        }
+                    },
+                    Responses = new()
+                    {
+                        ["200"] = new OpenApiResponse
+                        {
+                            Description = "OK",
+                            Content = new Dictionary<string, OpenApiMediaType>()
+                            {
+                                ["application/json"] = new OpenApiMediaType
+                                {
+                                    Schema = new OpenApiSchemaReference("Person", document),
+                                }
+                            }
+                        }
+                    }
+                },
+                [HttpMethod.Post] = new OpenApiOperation
+                {
+                    RequestBody = new OpenApiRequestBody
+                    {
+                        Content = new Dictionary<string, OpenApiMediaType>()
+                        {
+                            ["application/json"] = new OpenApiMediaType
+                            {
+                                Schema = new OpenApiSchemaReference("Person", document),
+                            }
+                        }
+                    },
+                    Responses = new()
+                    {
+                        ["200"] = new OpenApiResponse
+                        {
+                            Description = "OK",
+                            Content = new Dictionary<string, OpenApiMediaType>()
+                            {
+                                ["application/json"] = new OpenApiMediaType
+                                {
+                                    Schema = new OpenApiSchemaReference("Person", document),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        var ruleSet = ValidationRuleSet.GetDefaultRuleSet();
+
+        // Act
+        var warnings = document.Validate(ruleSet);
+        var result = !warnings.Any();
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(warnings);
+        Assert.Empty(warnings);
     }
 }
