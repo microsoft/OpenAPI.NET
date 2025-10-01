3645ba172022f77c08c1777d6ba7edc18ff75df2
costabello matthieu <mcostabello@outlook.com>
2025-10-01 13:14:36 -0400

    Add support for querystring parameter location

--- src/Microsoft.OpenApi/Models/OpenApiParameter.cs
+++ src/Microsoft.OpenApi/Models/OpenApiParameter.cs
@@ -116,6 +116,20 @@ namespace Microsoft.OpenApi
         {
             Utils.CheckArgumentNull(writer);
 
+            // Check for querystring restrictions
+            if (In == ParameterLocation.QueryString)
+            {
+                if (version < OpenApiSpecVersion.OpenApi3_2)
+                {
+                    throw new InvalidOperationException("Parameter location 'querystring' is only supported in OpenAPI 3.2.0 and above.");
+                }
+                // Only throw if forbidden properties are explicitly set (not just default values)
+                if ((_style.HasValue) || (_explode.HasValue && _explode.Value) || AllowReserved || Schema != null)
+                {
+                    throw new InvalidOperationException("When 'in' is 'querystring', 'style', 'explode', 'allowReserved', and 'schema' properties MUST NOT be used as per OpenAPI 3.2 specification.");
+                }
+            }
+
             writer.WriteStartObject();
 
             // name
@@ -251,6 +265,12 @@ namespace Microsoft.OpenApi
         public virtual void SerializeAsV2(IOpenApiWriter writer)
         {
             Utils.CheckArgumentNull(writer);
+
+            // Throw if 'querystring' is used in V2
+            if (In == ParameterLocation.QueryString)
+            {
+                throw new InvalidOperationException("Parameter location 'querystring' is not supported in OpenAPI 2.0.");
+            }
 
             writer.WriteStartObject();
 
