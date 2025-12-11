f9e524859722476b3111cb6006f77208c2d1f526
Vincent Biret <vibiret@microsoft.com>
2025-12-11 14:34:46 -0500

    fix: reading streams in an asp.net context would cause async exceptions

--- test/Microsoft.OpenApi.Tests/Reader/OpenApiModelFactoryTests.cs
+++ test/Microsoft.OpenApi.Tests/Reader/OpenApiModelFactoryTests.cs
@@ -120,11 +120,7 @@ $$$"""
         Assert.NotNull(readResult.Document.Components);
         Assert.Equal(baseUri, readResult.Document.BaseUri);
     }
-    [Fact]
-    public async Task CanLoadANonSeekableStream()
-    {
-        // Given
-        var documentJson = 
+    private readonly string documentJson =
 """
 {
     "openapi": "3.1.0",
@@ -135,6 +131,18 @@ $$$"""
     "paths": {}
 }
 """;
+    private readonly string documentYaml =
+"""
+openapi: 3.1.0
+info:
+  title: Sample API
+  version: 1.0.0
+paths: {}
+""";
+    [Fact]
+    public async Task CanLoadANonSeekableStreamInJsonAndDetectFormat()
+    {
+        // Given
         using var memoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(documentJson));
         using var nonSeekableStream = new NonSeekableStream(memoryStream);
     
@@ -146,6 +154,194 @@ $$$"""
         Assert.Equal("Sample API", document.Info.Title);
     }
 
+    [Fact]
+    public async Task CanLoadANonSeekableStreamInYamlAndDetectFormat()
+    {
+        // Given
+        using var memoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(documentYaml));
+        using var nonSeekableStream = new NonSeekableStream(memoryStream);
+        var settings = new OpenApiReaderSettings();
+        settings.AddYamlReader();
+    
+        // When
+        var (document, _) = await OpenApiDocument.LoadAsync(nonSeekableStream, settings: settings);
+    
+        // Then
+        Assert.NotNull(document);
+        Assert.Equal("Sample API", document.Info.Title);
+    }
+
+    [Fact]
+    public async Task CanLoadAnAsyncOnlyStreamInJsonAndDetectFormat()
+    {
+        // Given
+        await using var memoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(documentJson));
+        await using var nonSeekableStream = new AsyncOnlyStream(memoryStream);
+    
+        // When
+        var (document, _) = await OpenApiDocument.LoadAsync(nonSeekableStream);
+    
+        // Then
+        Assert.NotNull(document);
+        Assert.Equal("Sample API", document.Info.Title);
+    }
+
+    [Fact]
+    public async Task CanLoadAnAsyncOnlyStreamInYamlAndDetectFormat()
+    {
+        // Given
+        await using var memoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(documentYaml));
+        await using var nonSeekableStream = new AsyncOnlyStream(memoryStream);
+        var settings = new OpenApiReaderSettings();
+        settings.AddYamlReader();
+    
+        // When
+        var (document, _) = await OpenApiDocument.LoadAsync(nonSeekableStream, settings: settings);
+    
+        // Then
+        Assert.NotNull(document);
+        Assert.Equal("Sample API", document.Info.Title);
+    }
+
+    public sealed class AsyncOnlyStream : Stream
+    {
+        private readonly Stream _innerStream;
+        public AsyncOnlyStream(Stream stream) : base()
+        {
+            _innerStream = stream;
+        }
+        public override bool CanSeek => _innerStream.CanSeek;
+
+        public override long Position { get => _innerStream.Position; set => throw new NotSupportedException("Blocking operations are not supported"); }
+
+        public override bool CanRead => _innerStream.CanRead;
+
+        public override bool CanWrite => _innerStream.CanWrite;
+
+        public override long Length => _innerStream.Length;
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return _innerStream.BeginRead(buffer, offset, count, callback, state);
+        }
+
+        public override void Flush()
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override void SetLength(long value)
+        {
+            _innerStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await _innerStream.DisposeAsync();
+            await base.DisposeAsync();
+        }
+
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            return _innerStream.CopyToAsync(destination, bufferSize, cancellationToken);
+        }
+
+        public override bool CanTimeout => _innerStream.CanTimeout;
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return _innerStream.BeginWrite(buffer, offset, count, callback, state);
+        }
+
+        public override void CopyTo(Stream destination, int bufferSize)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override void Close()
+        {
+            _innerStream.Close();
+        }
+
+        public override int EndRead(IAsyncResult asyncResult)
+        {
+            return _innerStream.EndRead(asyncResult);
+        }
+
+        public override void EndWrite(IAsyncResult asyncResult)
+        {
+            _innerStream.EndWrite(asyncResult);
+        }
+
+        public override int ReadByte()
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override void WriteByte(byte value)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return _innerStream.FlushAsync(cancellationToken);
+        }
+
+        public override int Read(Span<byte> buffer)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return _innerStream.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            return _innerStream.ReadAsync(buffer, cancellationToken);
+        }
+
+        public override int ReadTimeout { get => _innerStream.ReadTimeout; set => _innerStream.ReadTimeout = value; }
+
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            throw new NotSupportedException("Blocking operations are not supported.");
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return _innerStream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+        public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            return _innerStream.WriteAsync(buffer, cancellationToken);
+        }
+
+        public override int WriteTimeout { get => _innerStream.WriteTimeout; set => _innerStream.WriteTimeout = value; }
+
+    }
+
     public sealed class NonSeekableStream : Stream
     {
         private readonly Stream _innerStream;
@@ -155,7 +351,7 @@ $$$"""
         }
         public override bool CanSeek => false;
 
-        public override long Position { get => _innerStream.Position; set => throw new InvalidOperationException("Seeking is not supported."); }
+        public override long Position { get => _innerStream.Position; set => throw new NotSupportedException("Seeking is not supported."); }
 
         public override bool CanRead => _innerStream.CanRead;
 
