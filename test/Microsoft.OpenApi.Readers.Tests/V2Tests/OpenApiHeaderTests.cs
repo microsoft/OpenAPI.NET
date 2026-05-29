54cf5dce303dafe7f7bc0505e8f551a50ceb9453
Vincent Biret <vibiret@microsoft.com>
2026-05-29 15:19:37 -0400

    chore: moves using after the copyright
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- test/Microsoft.OpenApi.Readers.Tests/V2Tests/OpenApiHeaderTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/V2Tests/OpenApiHeaderTests.cs
@@ -1,7 +1,7 @@
-using System.Text.Json.Nodes;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System.Text.Json.Nodes;
 using System.IO;
 using FluentAssertions;
 using FluentAssertions.Equivalency;
