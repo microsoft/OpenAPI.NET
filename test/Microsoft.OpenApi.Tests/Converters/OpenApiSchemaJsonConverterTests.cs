85be2dbd585b38cb44aa2678e53c648e553257e7
Vincent Biret <vibiret@microsoft.com>
2026-07-02 10:57:24 -0400

    chore: restores system import
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- test/Microsoft.OpenApi.Tests/Converters/OpenApiSchemaJsonConverterTests.cs
+++ test/Microsoft.OpenApi.Tests/Converters/OpenApiSchemaJsonConverterTests.cs
@@ -1,6 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using Xunit;
