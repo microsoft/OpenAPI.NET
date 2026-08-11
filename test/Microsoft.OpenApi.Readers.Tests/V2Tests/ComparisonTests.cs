acb80cac59c9ea9683665c22ad9c92c9b58f00fd
Vincent Biret <vibiret@microsoft.com>
2026-08-11 13:55:15 -0400

    linting: use path join instead of combine
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- test/Microsoft.OpenApi.Readers.Tests/V2Tests/ComparisonTests.cs
+++ test/Microsoft.OpenApi.Readers.Tests/V2Tests/ComparisonTests.cs
@@ -22,10 +22,10 @@ namespace Microsoft.OpenApi.Readers.Tests.V2Tests
         {
             var settings = new OpenApiReaderSettings();
             settings.AddYamlReader();
-            using var streamV2 = Resources.GetStream(Path.Combine(SampleFolderPath, $"{fileName}.v2.yaml"));
-            using var streamV3 = Resources.GetStream(Path.Combine(SampleFolderPath, $"{fileName}.v3.yaml"));
-            var result1 = await OpenApiDocument.LoadAsync(Path.Combine(SampleFolderPath, $"{fileName}.v2.yaml"), SettingsFixture.ReaderSettings, token: TestContext.Current.CancellationToken);
-            var result2 = await OpenApiDocument.LoadAsync(Path.Combine(SampleFolderPath, $"{fileName}.v3.yaml"), SettingsFixture.ReaderSettings, token: TestContext.Current.CancellationToken);
+            using var streamV2 = Resources.GetStream(Path.Join(SampleFolderPath, $"{fileName}.v2.yaml"));
+            using var streamV3 = Resources.GetStream(Path.Join(SampleFolderPath, $"{fileName}.v3.yaml"));
+            var result1 = await OpenApiDocument.LoadAsync(Path.Join(SampleFolderPath, $"{fileName}.v2.yaml"), SettingsFixture.ReaderSettings, token: TestContext.Current.CancellationToken);
+            var result2 = await OpenApiDocument.LoadAsync(Path.Join(SampleFolderPath, $"{fileName}.v3.yaml"), SettingsFixture.ReaderSettings, token: TestContext.Current.CancellationToken);
 
             result2.Document.Should().BeEquivalentTo(result1.Document,
                 options => options.Excluding(x => x.Workspace).Excluding(y => y.BaseUri));
