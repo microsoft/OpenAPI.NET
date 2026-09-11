33daae2a2dac97fa449633631f6afb9b0260115a
Vincent Biret <vibiret@microsoft.com>
2026-09-11 15:11:24 -0400

    chore: fixes wrong merge
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- test/Microsoft.OpenApi.Tests/Mocks/OpenApiResponseSerializationTests.cs
+++ test/Microsoft.OpenApi.Tests/Mocks/OpenApiResponseSerializationTests.cs
@@ -68,7 +68,6 @@ namespace Microsoft.OpenApi.Tests.Mocks
         [InlineData(OpenApiSpecVersion.OpenApi2_0)]
         [InlineData(OpenApiSpecVersion.OpenApi3_0)]
         [InlineData(OpenApiSpecVersion.OpenApi3_1)]
-        [InlineData(OpenApiSpecVersion.OpenApi3_2)]
         public async Task SerializeResponseWithoutDescriptionAsJsonDoesNotWriteNullDescription(OpenApiSpecVersion specVersion)
         {
             // Arrange
